09af3c6a287821c366a6b178a3ddb14322c950f3
Waad Moaness <waadmoaness99@gmail.com>
2026-03-26 19:11:35 +0200

    fix model caching

--- app/main.py
+++ app/main.py
@@ -13,4 +13,4 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=7860)
